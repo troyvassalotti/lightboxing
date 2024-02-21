3595cf8a4b06df150edd6deb92e9be624165b845
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2024-02-21 07:35:01 -0500

    Fix scripts

--- lightboxing.js
+++ lightboxing.js
@@ -1,4 +1,6 @@
+/** @format */
+
 import Lightbox from "./light-box.js";
 import LightboxCarousel from "./lightbox-carousel.js";
 
-export { Lightbox, LightboxCarousel };
+export {Lightbox, LightboxCarousel};
